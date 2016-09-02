d4a28f4eca655e362632ed7e04d6d0604a1bf6b6
Chris Smowton <chris@smowton.net>
2016-09-02 14:28:43 +0100

    Guess that other programs using remove-virtual will want the similar remove-instanceof too.

--- src/goto-programs/remove_instanceof.h
+++ src/goto-programs/remove_instanceof.h
@@ -4,10 +4,12 @@
 
 #include <util/symbol_table.h>
 #include "goto_functions.h"
+#include "goto_model.h"
 
 void remove_instanceof(
   symbol_tablet &symbol_table,
   goto_functionst &goto_functions);
 
+void remove_instanceof(goto_modelt &model);
 
 #endif
