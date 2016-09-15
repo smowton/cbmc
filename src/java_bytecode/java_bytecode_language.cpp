9add76c37ad777d075668129caff1a5099315a34
Cristina <cristina.david@gmail.com>
2016-09-15 17:52:33 +0100

    enable the runtime-check flag for now

--- src/java_bytecode/java_bytecode_language.cpp
+++ src/java_bytecode/java_bytecode_language.cpp
@@ -37,7 +37,7 @@ Function: java_bytecode_languaget::get_language_options
 
 void java_bytecode_languaget::get_language_options(const cmdlinet& cmd)
 {
-  enable_runtime_checks==cmd.isset("runtime-check");
+  enable_runtime_checks=true; //cmd.isset("runtime-check");
   assume_inputs_non_null=cmd.isset("java-assume-inputs-non-null");
   if(cmd.isset("java-max-input-array-length"))
     max_nondet_array_length=safe_string2int(cmd.get_value("java-max-input-array-length"));
