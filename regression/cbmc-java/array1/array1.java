f62964c7fa04eb8511321290667348d92e8f6017
Pascal Kesseli <pkesseli@dkr13.cs.ox.ac.uk>
2016-09-30 14:18:31 +0100

    Extended do_java_new_array with explicit case for constant size expr (ARRAY_SET).

--- regression/cbmc-java/array1/array1.java
+++ regression/cbmc-java/array1/array1.java
@@ -3,12 +3,16 @@ class array1
   public static void main(String[] args)
   {
     int size=10;
-    int int_array[]=new int[size];
+    int int_array[]=new int[10];
+
+    int any_index = new java.util.Random().nextInt();
+    if (any_index < 0 || any_index >= size) return;
+    assert int_array[any_index] == 0;
     
     for(int i=0; i<size; i++)
       int_array[i]=i;
 
-    assert int_array[7] == 7;
+    assert int_array[any_index] == any_index;
 
     what_not what_not_array[]=new what_not[size];
     
