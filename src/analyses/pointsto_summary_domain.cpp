01569ba35ffabe3f7c3aaa92540d98a005359bec
Marek Trtik <trtik@diffblue.com>
2016-10-11 19:18:02 +0100

    Finished interface of points-to summary domain. (Documentation is still needed)

--- src/analyses/pointsto_summary_domain.cpp
+++ src/analyses/pointsto_summary_domain.cpp
@@ -14,3 +14,255 @@ Date: Octomber 2016
 #include <analyses/pointsto_summary_domain.h>
 
 
+pointsto_expressiont::pointsto_expressiont(const dstring&  keyword)
+  : irept(keyword)
+{}
+
+dstring pointsto_expressiont::keyword()
+{
+  return "";
+}
+
+
+dstring pointsto_set_of_offsetst::keyword()
+{
+  return ID_pointsto_access_paths_definition_offsets;
+}
+
+pointsto_set_of_offsetst::pointsto_set_of_offsetst(
+    const offset_namest&  offset_names,
+    bool const is_exact
+    )
+  : irept(keyword())
+{
+  get_sub().push_back(irept(is_exact ? "yes" : "no"));
+  for (auto const&  offset : offset_names)
+    get_sub().push_back(irept(offset));
+}
+
+void  pointsto_set_of_offsetst::get_offset_names(
+    offset_namest&  output_names
+    ) const
+{
+  for (std::size_t  i = 1UL; i < get_sub().size(); ++i)
+    output_names.insert(get_sub().at(i).id());
+}
+
+bool  pointsto_set_of_offsetst::is_exact() const
+{
+  return get_sub().front().id() == "yes";
+}
+
+
+dstring pointsto_address_shiftt::keyword()
+{
+  return ID_pointsto_access_paths_shift;
+}
+
+pointsto_address_shiftt::pointsto_address_shiftt(
+    const pointsto_expressiont&  targets,
+    const pointsto_set_of_offsetst&  offsets
+    )
+  : irept(keyword())
+{
+  get_sub().push_back(targets);
+  get_sub().push_back(offsets);
+}
+
+const pointsto_expressiont& pointsto_address_shiftt::get_targets() const
+{
+  return pointsto_as<pointsto_expressiont>(get_sub().front());
+}
+
+const pointsto_set_of_offsetst& pointsto_address_shiftt::get_offsets() const
+{
+  return pointsto_as<pointsto_set_of_offsetst>(get_sub().back());
+}
+
+
+dstring pointsto_symbolic_set_of_targetst::keyword()
+{
+  return ID_pointsto_access_paths_definition_symbolic;
+}
+
+pointsto_symbolic_set_of_targetst::pointsto_symbolic_set_of_targetst()
+  : pointsto_expressiont(keyword())
+{
+  get_sub().push_back(irept(make_fresh_symbol_name()));
+}
+
+const irep_idt& pointsto_symbolic_set_of_targetst::get_symbolic_set_name() const
+{
+  return get_sub().back().id();
+}
+
+irep_idt  pointsto_symbolic_set_of_targetst::make_fresh_symbol_name()
+{
+  static uint64_t  counter = 0UL;
+  std::string const  symbol_name =
+      msgstream() << "A" << ++counter;
+  return symbol_name;
+}
+
+
+dstring  pointsto_set_of_concrete_targetst::keyword()
+{
+  return ID_pointsto_access_paths_definition_concrete;
+}
+
+pointsto_set_of_concrete_targetst::pointsto_set_of_concrete_targetst(
+    const irep_idt&  function_name,
+    const unsigned int  location_number,
+    const irep_idt&  target_name
+    )
+  : pointsto_set_of_concrete_targetst({
+          concrete_targett(function_name,location_number,target_name)
+          })
+{}
+
+pointsto_set_of_concrete_targetst::pointsto_set_of_concrete_targetst(
+    const concrete_targetst&  targets
+    )
+  : pointsto_expressiont(keyword())
+{
+  for (const auto&  target : targets)
+  {
+    irept  concrete("concrete");
+    concrete.get_sub().push_back(irept(std::get<0>(target)));
+    concrete.get_sub().push_back(irept(
+            msgstream() << std::get<1>(target) << msgstream::end()
+            ));
+    concrete.get_sub().push_back(irept(std::get<2>(target)));
+
+    get_sub().push_back(concrete);
+  }
+}
+
+std::size_t  pointsto_set_of_concrete_targetst::get_num_targets() const
+{
+  return get_sub().size();
+}
+
+const irep_idt&  pointsto_set_of_concrete_targetst::get_function_name(
+    const std::size_t target_index
+    ) const
+{
+  return get_sub().at(target_index).get_sub().at(0).id();
+}
+
+unsigned int  pointsto_set_of_concrete_targetst::get_location_number(
+    const std::size_t target_index
+    ) const
+{
+  std::stringstream sstr(
+        as_string(get_sub().at(target_index).get_sub().at(1).id())
+        );
+  unsigned int  result;
+  sstr >> result;
+  return result;
+}
+
+const irep_idt&  pointsto_set_of_concrete_targetst::get_target_name(
+    const std::size_t target_index
+    ) const
+{
+  return get_sub().at(target_index).get_sub().at(2).id();
+}
+
+
+dstring pointsto_address_dereferencet::keyword()
+{
+  return ID_pointsto_access_paths_dereference;
+}
+
+pointsto_address_dereferencet::pointsto_address_dereferencet(
+    const pointsto_address_shiftt&  address_shift
+    )
+  : pointsto_expressiont(keyword())
+{
+  get_sub().push_back(address_shift);
+}
+
+const pointsto_address_shiftt&
+pointsto_address_dereferencet::get_address_shift() const
+{
+  return pointsto_as<pointsto_address_shiftt>(get_sub().front());
+}
+
+
+dstring pointsto_subtract_sets_of_targetst::keyword()
+{
+  return ID_pointsto_access_paths_subtract;
+}
+
+pointsto_subtract_sets_of_targetst::pointsto_subtract_sets_of_targetst(
+    const pointsto_expressiont&  left,
+    const pointsto_expressiont&  right
+    )
+  : pointsto_expressiont(keyword())
+{
+  get_sub().push_back(left);
+  get_sub().push_back(right);
+}
+
+const pointsto_expressiont& pointsto_subtract_sets_of_targetst::get_left() const
+{
+  return pointsto_as<pointsto_expressiont>(get_sub().front());
+}
+
+const pointsto_expressiont& pointsto_subtract_sets_of_targetst::get_right()const
+{
+  return pointsto_as<pointsto_expressiont>(get_sub().back());
+}
+
+
+dstring pointsto_union_sets_of_targetst::keyword()
+{
+  return ID_pointsto_access_paths_union;
+}
+
+pointsto_union_sets_of_targetst::pointsto_union_sets_of_targetst(
+    const pointsto_expressiont&  left,
+    const pointsto_expressiont&  right
+    )
+  : pointsto_expressiont(keyword())
+{
+  get_sub().push_back(left);
+  get_sub().push_back(right);
+}
+
+const pointsto_expressiont& pointsto_union_sets_of_targetst::get_left() const
+{
+  return pointsto_as<pointsto_expressiont>(get_sub().front());
+}
+
+const pointsto_expressiont& pointsto_union_sets_of_targetst::get_right()const
+{
+  return pointsto_as<pointsto_expressiont>(get_sub().back());
+}
+
+
+pointsto_map_from_pointers_to_targetst::pointsto_map_from_pointers_to_targetst(
+    const pointsto_expressiont&  set_of_pointers_,
+    const pointsto_expressiont&  set_of_targets_
+    )
+  : set_of_pointers(set_of_pointers_)
+  , set_of_targets(set_of_targets_)
+{}
+
+
+pointsto_summaryt::pointsto_summaryt(
+    const pointsto_map_from_pointers_to_targetst&  mapping_of_symbols_,
+    const std::vector<pointsto_map_from_pointers_to_targetst>&  rules_
+    )
+  : mapping_of_symbols(mapping_of_symbols_)
+  , rules(rules_)
+{}
+
+
+
+
+
+
+
+
--- src/analyses/pointsto_summary_domain.h
+++ src/analyses/pointsto_summary_domain.h
@@ -23,153 +23,206 @@ Date: Octomber 2016
 #include <unordered_set>
 #include <sstream>
 #include <string>
+#include <tuple>
+#include <tuple>
 
-class pointsto_symbolic_set_of_targetst : public irept
+
+class pointsto_expressiont;
+
+template<typename T>
+class pointesto_detail_is_expression
 {
 public:
-  static const dstring keyword()
-  { return ID_pointsto_access_paths_definition_symbolic; }
-
-  pointsto_symbolic_set_of_targetst(const irep_idt&  symbolic_set_name)
-    : irept(keyword())
-  {
-    get_sub().push_back(irept(symbolic_set_name));
-  }
-
-  const irep_idt&  get_symbolic_set_name() const
-  { return get_sub().back().id(); }
+  static const bool value = false;
 };
 
-class pointsto_set_of_concrete_targetst : public irept
+template<>
+class pointesto_detail_is_expression<pointsto_expressiont>
 {
 public:
-  static const dstring keyword()
-  { return ID_pointsto_access_paths_definition_concrete; }
+  static const bool value = true;
+};
 
+
+template<typename T>
+inline bool  pointsto_is_of(const irept& irep)
+{
+  return pointesto_detail_is_expression<T>::value || irep.id() == T::keyword();
+}
+
+template<typename T>
+inline const T&  pointsto_as(const irept& irep)
+{
+  assert(pointsto_is_of<T>(irep));
+  return static_cast<const T&>(irep);
+}
+
+
+class pointsto_expressiont : public irept
+{
+public:
+  static dstring keyword();
+  pointsto_expressiont(const dstring&  keyword);
+};
+
+
+class pointsto_set_of_offsetst : public irept
+{
+public:
+  typedef std::unordered_set<irep_idt,dstring_hash>  offset_namest;
+
+  static dstring keyword();
+
+  pointsto_set_of_offsetst(
+      const offset_namest&  offset_names,
+      bool const is_exact
+      );
+
+  void  get_offset_names(offset_namest&  output_names) const;
+  bool  is_exact() const;
+};
+
+
+class pointsto_address_shiftt
+    : public irept
+{
+public:
+  static dstring keyword();
+
+  pointsto_address_shiftt(
+      const pointsto_expressiont&  targets,
+      const pointsto_set_of_offsetst&  offsets
+      );
+
+  const pointsto_expressiont&  get_targets() const;
+  const pointsto_set_of_offsetst&  get_offsets() const;
+};
+
+
+class pointsto_symbolic_set_of_targetst
+    : public pointsto_expressiont
+{
+public:
+  static dstring keyword();
+
+  pointsto_symbolic_set_of_targetst();
+  const irep_idt&  get_symbolic_set_name() const;
+
+private:
+  static irep_idt  make_fresh_symbol_name();
+};
+
+
+class pointsto_set_of_concrete_targetst
+    : public pointsto_expressiont
+{
+public:
   typedef std::tuple<irep_idt,unsigned int,irep_idt>  concrete_targett;
   typedef std::vector<concrete_targett>  concrete_targetst;
+
+  static dstring keyword();
 
   pointsto_set_of_concrete_targetst(
       const irep_idt&  function_name,
       const unsigned int  location_number,
       const irep_idt&  target_name
-      )
-    : pointsto_set_of_concrete_targetst({
-            concrete_targett(function_name,location_number,target_name)
-            })
-  {}
+      );
+  pointsto_set_of_concrete_targetst(const concrete_targetst&  targets);
 
-  pointsto_set_of_concrete_targetst(const concrete_targetst&  targets)
-    : irept(keyword())
-  {
-    for (const auto&  target : targets)
-    {
-      irept  concrete("concrete");
-      concrete.get_sub().push_back(irept(std::get<0>(target)));
-      concrete.get_sub().push_back(irept(
-              msgstream() << std::get<1>(target) << msgstream::end()
-              ));
-      concrete.get_sub().push_back(irept(std::get<2>(target)));
-
-      get_sub().push_back(concrete);
-    }
-  }
-
-  std::size_t  get_num_targets() const { return get_sub().size(); }
-
-  const irep_idt&  get_function_name(const std::size_t target_index) const
-  { return get_sub().at(target_index).get_sub().at(0).id(); }
-
-  unsigned int  get_location_number(const std::size_t target_index) const
-  {
-    std::stringstream sstr(
-          as_string(get_sub().at(target_index).get_sub().at(1).id())
-          );
-    unsigned int  result;
-    sstr >> result;
-    return result;
-  }
-
-  const irep_idt&  get_target_name(const std::size_t target_index) const
-  { return get_sub().at(target_index).get_sub().at(2).id(); }
+  std::size_t  get_num_targets() const;
+  const irep_idt&  get_function_name(const std::size_t target_index) const;
+  unsigned int  get_location_number(const std::size_t target_index) const;
+  const irep_idt&  get_target_name(const std::size_t target_index) const;
 };
 
-class pointsto_set_of_offsetst : public irept
+
+class pointsto_address_dereferencet
+    : public pointsto_expressiont
 {
 public:
-  static const dstring keyword()
-  { return ID_pointsto_access_paths_definition_offsets; }
+  static dstring keyword();
 
-  typedef std::unordered_set<irep_idt,dstring_hash>  offset_namest;
+  pointsto_address_dereferencet(
+      const pointsto_address_shiftt&  address_shift
+      );
 
-  pointsto_set_of_offsetst(const offset_namest&  offset_names)
-    : irept(keyword())
-  {
-    for (auto const&  offset : offset_names)
-      get_sub().push_back(irept(offset));
-  }
-
-  void  get_offset_names(offset_namest&  output_names)
-  {
-    for (const irept&  offset : get_sub())
-      output_names.insert(offset.id());
-  }
+  const pointsto_address_shiftt&  get_address_shift() const;
 };
 
-class pointsto_address_shiftt : public irept
+
+class pointsto_subtract_sets_of_targetst
+    : public pointsto_expressiont
 {
 public:
-  static const dstring keyword()
-  { return ID_pointsto_access_paths_shift; }
+  static dstring keyword();
 
-  pointsto_address_shiftt(
-      const pointsto_symbolic_set_of_targetst&  targets,
-      const pointsto_set_of_offsetst&  offsets
-      )
-    : irept(keyword())
-  {
-    get_sub().push_back(targets);
-    get_sub().push_back(get_nil_irep());
-    get_sub().push_back(offsets);
-  }
+  pointsto_subtract_sets_of_targetst(
+      const pointsto_expressiont&  left,
+      const pointsto_expressiont&  right
+      );
 
-  pointsto_address_shiftt(
-      const pointsto_set_of_concrete_targetst&  targets,
-      const pointsto_set_of_offsetst&  offsets
-      )
-    : irept(keyword())
-  {
-    get_sub().push_back(get_nil_irep());
-    get_sub().push_back(targets);
-    get_sub().push_back(offsets);
-  }
-
-  bool  has_symbolic_targets() const
-  { return !(get_sub().front() == get_nil_irep()); }
-
-  bool  has_concrete_targets() const { return !has_symbolic_targets(); }
-
-  const pointsto_symbolic_set_of_targetst&  get_symbolic_targets() const
-  {
-    return static_cast<const pointsto_symbolic_set_of_targetst&>(
-                get_sub().front()
-                );
-  }
-
-  const pointsto_set_of_concrete_targetst&  get_concrete_targets() const
-  {
-    return static_cast<const pointsto_set_of_concrete_targetst&>(
-                get_sub().at(1)
-                );
-  }
-
-  const pointsto_set_of_offsetst&  get_offsets() const
-  {
-    return static_cast<const pointsto_set_of_offsetst&>(
-                get_sub().back()
-                );
-  }
+  const pointsto_expressiont&  get_left() const;
+  const pointsto_expressiont&  get_right() const;
 };
 
+
+class pointsto_union_sets_of_targetst
+    : public pointsto_expressiont
+{
+public:
+  static dstring keyword();
+
+  pointsto_union_sets_of_targetst(
+      const pointsto_expressiont&  left,
+      const pointsto_expressiont&  right
+      );
+
+  const pointsto_expressiont&  get_left() const;
+  const pointsto_expressiont&  get_right() const;
+};
+
+
+class pointsto_map_from_pointers_to_targetst
+{
+public:
+
+  pointsto_map_from_pointers_to_targetst(
+      const pointsto_expressiont&  set_of_pointers_,
+      const pointsto_expressiont&  set_of_targets_
+      );
+
+  const pointsto_expressiont&  get_set_of_pointers() const noexcept
+  { return set_of_pointers; }
+
+  const pointsto_expressiont&  get_set_of_targets() const noexcept
+  { return set_of_targets; }
+
+private:
+  pointsto_expressiont  set_of_pointers;
+  pointsto_expressiont  set_of_targets;
+};
+
+
+class pointsto_summaryt
+{
+public:
+
+  pointsto_summaryt(
+      const pointsto_map_from_pointers_to_targetst&  mapping_of_symbols_,
+      const std::vector<pointsto_map_from_pointers_to_targetst>&  rules_
+      );
+
+  const pointsto_map_from_pointers_to_targetst&
+  get_mapping_of_symbols() const noexcept
+  { return mapping_of_symbols; }
+
+  const std::vector<pointsto_map_from_pointers_to_targetst>&
+  get_rules() const noexcept
+  { return rules; }
+
+private:
+  pointsto_map_from_pointers_to_targetst  mapping_of_symbols;
+  std::vector<pointsto_map_from_pointers_to_targetst>  rules;
+};
 
 #endif
