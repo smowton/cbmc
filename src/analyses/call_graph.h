433c60b952cb650beb4de4ed05973fd83defc256
Chris Smowton <chris@smowton.net>
2017-03-08 10:21:07 +0000

    Style and document call graph

--- src/analyses/call_graph.h
+++ src/analyses/call_graph.h
@@ -118,7 +118,7 @@ Typical usage:
    std::cout << fn_name << ", ";
 
 \*******************************************************************/
-void  inverted_partial_topological_order(
+void inverted_partial_topological_order(
   call_grapht const &call_graph,
   irep_idt const &start_function,
   std::unordered_set<irep_idt, dstring_hash> &processed_functions,
@@ -129,18 +129,18 @@ void get_inverted_topological_order(
   goto_functionst const& functions,
   std::vector<irep_idt>& output);
 
-bool  exists_direct_call(
+bool exists_direct_call(
   call_grapht const &call_graph,
   irep_idt const &caller,
   irep_idt const &callee);
 
-bool  exists_direct_or_indirect_call(
+bool exists_direct_or_indirect_call(
   call_grapht const &call_graph,
   irep_idt const &caller,
   irep_idt const &callee,
   std::unordered_set<irep_idt, dstring_hash> &ignored_functions);
 
-bool  exists_direct_or_indirect_call(
+bool exists_direct_or_indirect_call(
   call_grapht const &call_graph,
   irep_idt const &caller,
   irep_idt const &callee);
