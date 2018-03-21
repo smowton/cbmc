bf075a3d6fc6e695f7b188fb4821e0915e93ec99
Chris Smowton <chris@smowton.net>
2018-03-21 18:01:34 +0000

    Add format-classpath script, and convert tests to use it

---
+++ regression/cbmc-java/overlay-class/format_classpath.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+unameOut="$(uname -s)"
+case "${unameOut}" in
+    CYGWIN*)    separator=";";;
+    MINGW*)     separator=";";;
+    MSYS*)      separator=";";;
+    Windows*)   separator=";";;
+    *)          separator=":"
+esac
+
+echo -n "--classpath "
+echo -n `IFS=$separator; echo "$*"`
