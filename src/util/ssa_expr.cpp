b33c45cad6acd8407a5d4f5392f289c634517adb
Chris Smowton <chris@smowton.net>
2018-12-18 10:11:29 +0000

    Represent constant-array-index SSA expressions using notation array[[index]]
    This makes it possible to distinguish such SSA symbols from actual index_exprt in pretty-printed output.

--- src/util/ssa_expr.cpp
+++ src/util/ssa_expr.cpp
@@ -41,8 +41,8 @@ static void build_ssa_identifier_rec(
     build_ssa_identifier_rec(index.array(), l0, l1, l2, os, l1_object_os);
 
     const mp_integer idx = numeric_cast_v<mp_integer>(index.index());
-    os << '[' << idx << ']';
-    l1_object_os << '[' << idx << ']';
+    os << "[[" << idx << "]]";
+    l1_object_os << "[[" << idx << "]]";
   }
   else if(expr.id()==ID_symbol)
   {
